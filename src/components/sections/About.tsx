f4eab3876088053dbf84e148f3eecd934a0bbd1e
PinkGlove <gallagherguo@gmail.com>
2025-08-05 15:48:02 -0500

    Am I on the correct account now?

--- src/components/sections/About.tsx
+++ src/components/sections/About.tsx
@@ -2,7 +2,7 @@ export default function About() {
   return (
     <section>
       <div>
-        About Section - Coming Soon
+        About Section - Coming Soon!
       </div>
     </section>
   );
